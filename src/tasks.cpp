9e9c1b564c06d9a169cf3c8eace59846cd6bffa8
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2020-05-18 17:52:55 +0200

    Add National Instruments tasks

---
+++ src/tasks.cpp
@@ -0,0 +1,212 @@
+#include "tasks.h"
+
+Tasks::Tasks(QObject *parent) : QObject(parent)
+{
+    mainTrigger = new NITask("mainTrigger", this);
+    shutterPulse = new NITask("shutterPulse", this);
+    elReadout = new NITask("electrodeReadout", this);
+
+    needsInit = true;
+}
+
+void Tasks::init()
+{
+    QList<NITask *> taskList;
+
+    taskList << mainTrigger << shutterPulse << elReadout;
+
+    for (NITask *t : taskList) {
+        if (t->isInitialized())
+            t->clearTask();
+    }
+
+
+    // mainTrigger
+    // we have to use an AO for the main trigger because this board has only 2
+    // counters and they are both used for the SampMode_FiniteSamps in task
+    // shutterCO
+    mainTrigger->createTask();
+    mainTrigger->createAOVoltageChan(mainTrigPhysChan.toLatin1(),
+                                     nullptr,
+                                     0, 5, // range
+                                     NITask::VoltUnits_Volts, nullptr);
+
+    // because there are 2 samples, the sampling rate is double the wanted
+    // square wave frequency
+    mainTrigger->cfgSampClkTiming("OnboardClock",
+                                  2 * mainTrigFreq,
+                                  NITask::Edge_Rising,
+                                  NITask::SampMode_ContSamps, 2);
+    const float64 data[] = {0, 5};
+    int32 sampsPerChanWritten;
+    mainTrigger->writeAnalogF64(2,      // number of samples
+                                false,  // autostart
+                                .2,     // timeout
+                                NITask::DataLayout_GroupByChannel,
+                                data,
+                                &sampsPerChanWritten);
+    if (sampsPerChanWritten != 2)
+        throw std::runtime_error("invalid number of samples written");
+
+    QString startTriggerSOurce = mainTrigPhysChan + "/StartTrigger";
+
+
+
+    // shutterPulse
+    shutterPulse->createTask();
+    shutterPulse->createCOPulseChanFreq(
+        shutterPulseCounter.toLatin1(),
+        nullptr,
+        DAQmx_Val_Hz,
+        NITask::IdleState_Low,
+        shutterInitialDelay,
+        shutterPulseFrequency,
+        shutterPulseDuty);
+    shutterPulse->setCOPulseTerm(nullptr, shutterPulseTerm.toLatin1());
+    shutterPulse->cfgImplicitTiming(NITask::SampMode_FiniteSamps,
+                                    shutterPulseNPulses);
+    shutterPulse->cfgDigEdgeStartTrig(startTriggerSOurce.toLatin1(),
+                                      NITask::Edge_Rising);
+
+
+
+    // electrodeReadout
+    elReadout->createTask();
+    elReadout->createAIVoltageChan(electrodeReadoutPhysChan.toLatin1(),
+                                   nullptr,
+                                   NITask::TermConf_Default,
+                                   -10., 10.,
+                                   NITask::VoltUnits_Volts, nullptr);
+    elReadout->cfgSampClkTiming(
+        "OnboardClock",
+        electrodeReadoutRate,
+        NITask::Edge_Rising,
+        NITask::SampMode_ContSamps,
+        2 * electrodeReadoutRate);  // 2s worth of buffering
+    elReadout->cfgDigEdgeStartTrig(startTriggerSOurce.toLatin1(),
+                                   NITask::Edge_Rising);
+
+    needsInit = false;
+}
+
+NITask *Tasks::electrodeReadout()
+{
+    return elReadout;
+}
+
+void Tasks::start()
+{
+    if (needsInit)
+        init();
+    shutterPulse->startTask();
+    elReadout->startTask();
+
+    // started for last because it will trigger the other two tasks
+    mainTrigger->startTask();
+}
+
+void Tasks::stop()
+{
+    mainTrigger->stopTask();
+    shutterPulse->stopTask();
+    elReadout->stopTask();
+}
+
+QString Tasks::getMainTrigPhysChan() const
+{
+    return mainTrigPhysChan;
+}
+
+void Tasks::setMainTrigPhysChan(const QString &value)
+{
+    needsInit = true;
+    mainTrigPhysChan = value;
+}
+
+double Tasks::getMainTrigFreq() const
+{
+    return mainTrigFreq;
+}
+
+void Tasks::setMainTrigFreq(double value)
+{
+    needsInit = true;
+    mainTrigFreq = value;
+}
+
+QString Tasks::getShutterPulseCounter() const
+{
+    return shutterPulseCounter;
+}
+
+void Tasks::setShutterPulseCounter(const QString &value)
+{
+    needsInit = true;
+    shutterPulseCounter = value;
+}
+
+QString Tasks::getShutterPulseTerm() const
+{
+    return shutterPulseTerm;
+}
+
+void Tasks::setShutterPulseTerm(const QString &value)
+{
+    needsInit = true;
+    shutterPulseTerm = value;
+}
+
+double Tasks::getShutterPulseFrequency() const
+{
+    return shutterPulseFrequency;
+}
+
+void Tasks::setShutterPulseFrequency(double value)
+{
+    needsInit = true;
+    shutterPulseFrequency = value;
+}
+
+double Tasks::getShutterPulseDuty() const
+{
+    return shutterPulseDuty;
+}
+
+void Tasks::setShutterPulseDuty(double value)
+{
+    needsInit = true;
+    shutterPulseDuty = value;
+}
+
+uInt64 Tasks::getShutterPulseNPulses() const
+{
+    return shutterPulseNPulses;
+}
+
+void Tasks::setShutterPulseNPulses(const uInt64 &value)
+{
+    needsInit = true;
+    shutterPulseNPulses = value;
+}
+
+QString Tasks::getElectrodeReadoutPhysChan() const
+{
+    return electrodeReadoutPhysChan;
+}
+
+void Tasks::setElectrodeReadoutPhysChan(const QString &value)
+{
+    needsInit = true;
+    electrodeReadoutPhysChan = value;
+}
+
+double Tasks::getElectrodeReadoutRate() const
+{
+    return electrodeReadoutRate;
+}
+
+void Tasks::setElectrodeReadoutRate(double value)
+{
+    needsInit = true;
+    electrodeReadoutRate = value;
+}
---
+++ src/tasks.h
@@ -0,0 +1,73 @@
+#ifndef ELECTRODEREADOUT_H
+#define ELECTRODEREADOUT_H
+
+#include <QObject>
+
+#include <qtlab/hw/ni/nitask.h>
+
+class Tasks : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Tasks(QObject *parent = nullptr);
+    void init();
+
+    NITask *electrodeReadout();
+
+    QString getMainTrigPhysChan() const;
+    void setMainTrigPhysChan(const QString &value);
+
+    double getMainTrigFreq() const;
+    void setMainTrigFreq(double value);
+
+    QString getShutterPulseCounter() const;
+    void setShutterPulseCounter(const QString &value);
+
+    QString getShutterPulseTerm() const;
+    void setShutterPulseTerm(const QString &value);
+
+    double getShutterPulseFrequency() const;
+    void setShutterPulseFrequency(double value);
+
+    double getShutterPulseDuty() const;
+    void setShutterPulseDuty(double value);
+
+    uInt64 getShutterPulseNPulses() const;
+    void setShutterPulseNPulses(const uInt64 &value);
+
+    QString getElectrodeReadoutPhysChan() const;
+    void setElectrodeReadoutPhysChan(const QString &value);
+
+    double getElectrodeReadoutRate() const;
+    void setElectrodeReadoutRate(double value);
+
+signals:
+    void started();
+    void stopped();
+
+public slots:
+    void start();
+    void stop();
+
+private:
+    NITask *mainTrigger;
+    NITask *shutterPulse;
+    NITask *elReadout;
+
+    QString mainTrigPhysChan;
+    double mainTrigFreq;
+
+    QString shutterPulseCounter;
+    QString shutterPulseTerm;
+    double shutterInitialDelay = 10;
+    double shutterPulseFrequency = 20;
+    double shutterPulseDuty = 0.5;
+    uInt64 shutterPulseNPulses = 20;
+
+    QString electrodeReadoutPhysChan;
+    double electrodeReadoutRate = 10000;
+
+    bool needsInit;
+};
+
+#endif // ELECTRODEREADOUT_H
