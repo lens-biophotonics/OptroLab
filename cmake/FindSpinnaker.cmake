99a27aa8cd71eb3dea608c1fd21b78ff061627de
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2020-07-07 10:54:41 +0200

    cmake: fix FindSpinnaker, find SpinVideo

--- cmake/FindSpinnaker.cmake
+++ cmake/FindSpinnaker.cmake
@@ -15,11 +15,22 @@ find_library(Spinnaker_LIBRARY
     "${WIN_Spinnaker_DIR}/lib64"
 )
 
+find_library(SpinVideo_LIBRARY
+    NAMES SpinVideo
+    PATHS
+    /usr/lib/x86_64-linux-gnu
+    /usr/lib
+    /usr/local/lib
+    "$ENV{LIB_DIR}/lib"
+    "$ENV{LIB}"
+    "${WIN_Spinnaker_DIR}/lib64"
+)
+
 find_path(Spinnaker_INCLUDE_DIR NAMES Spinnaker.h PATHS
     "${INCLUDE_DIRS}"
     /usr/include
+    /usr/include/spinnaker
     /usr/local/include
-    /usr/local/hamamatsu_dcam/sdk/include/
     "$ENV{LIB_DIR}/include"
     "$ENV{INCLUDE}"
     "${WIN_Spinnaker_DIR}/include"
@@ -31,7 +42,7 @@ endif ()
 
 if (Spinnaker_INCLUDE_DIR AND Spinnaker_LIBRARY)
     set(Spinnaker_FOUND TRUE)
-    message(STATUS "Found ${Spinnaker_LIBNAME_STR}: ${Spinnaker_LIBRARY}")
+    message(STATUS "Found ${Spinnaker_LIBNAME_STR}: ${Spinnaker_LIBRARY} ${SpinVideo_LIBRARY}")
 endif ()
 
 if (NOT Spinnaker_FOUND AND Spinnaker_FIND_REQUIRED)
