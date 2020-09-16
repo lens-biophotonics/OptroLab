b40a5cef10b780fb404e6e796917d1ddb62b0049
Giacomo Mazzamuto <giacomo.mazzamuto@cnr.it>
2020-09-16 11:06:08 +0200

    save/load settings: use variable for better readability

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -104,32 +104,42 @@ void MainWindow::saveSettings() const
     Settings s = settings();
 
     Tasks *t = optrode().NITasks();
-    s.setValue(SETTINGSGROUP_MAINTRIG, SETTING_PHYSCHAN, t->getMainTrigPhysChan());
-    s.setValue(SETTINGSGROUP_MAINTRIG, SETTING_TERM, t->getMainTrigTerm());
 
-    s.setValue(SETTINGSGROUP_BEHAVCAMROI, SETTING_ROI, optrode().getBehaviorCamera()->getROI());
+    QString g = SETTINGSGROUP_MAINTRIG;
+    s.setValue(g, SETTING_PHYSCHAN, t->getMainTrigPhysChan());
+    s.setValue(g, SETTING_TERM, t->getMainTrigTerm());
 
-    s.setValue(SETTINGSGROUP_LED1, SETTING_FREQ, t->getLEDFreq());
-    s.setValue(SETTINGSGROUP_LED1, SETTING_PHYSCHAN, t->getLED1PhysChan());
-    s.setValue(SETTINGSGROUP_LED1, SETTING_TERM, t->getLED1Term());
-    s.setValue(SETTINGSGROUP_LED2, SETTING_PHYSCHAN, t->getLED2PhysChan());
-    s.setValue(SETTINGSGROUP_LED2, SETTING_TERM, t->getLED2Term());
+    g = SETTINGSGROUP_BEHAVCAMROI;
+    s.setValue(g, SETTING_ROI, optrode().getBehaviorCamera()->getROI());
 
-    s.setValue(SETTINGSGROUP_ELREADOUT, SETTING_PHYSCHAN, t->getElectrodeReadoutPhysChan());
-    s.setValue(SETTINGSGROUP_ELREADOUT, SETTING_FREQ, t->getElectrodeReadoutRate());
+    g = SETTINGSGROUP_LED1;
+    s.setValue(g, SETTING_FREQ, t->getLEDFreq());
+    s.setValue(g, SETTING_PHYSCHAN, t->getLED1PhysChan());
+    s.setValue(g, SETTING_TERM, t->getLED1Term());
 
-    s.setValue(SETTINGSGROUP_SHUTTER, SETTING_PHYSCHAN, t->getShutterPulseCounter());
-    s.setValue(SETTINGSGROUP_SHUTTER, SETTING_DUTY, t->getShutterPulseDuty());
-    s.setValue(SETTINGSGROUP_SHUTTER, SETTING_FREQ, t->getShutterPulseFrequency());
-    s.setValue(SETTINGSGROUP_SHUTTER, SETTING_TERM, t->getShutterPulseTerm());
-    s.setValue(SETTINGSGROUP_SHUTTER, SETTING_ENABLED, t->getShutterPulseEnabled());
+    g = SETTINGSGROUP_LED2;
+    s.setValue(g, SETTING_PHYSCHAN, t->getLED2PhysChan());
+    s.setValue(g, SETTING_TERM, t->getLED2Term());
 
-    s.setValue(SETTINGSGROUP_TIMING, SETTING_INITIALDELAY, t->getShutterInitialDelay());
-    s.setValue(SETTINGSGROUP_TIMING, SETTING_STIMDURATION, t->stimulationDuration());
-    s.setValue(SETTINGSGROUP_TIMING, SETTING_POSTSTIMULATION, optrode().getPostStimulation());
+    g = SETTINGSGROUP_ELREADOUT;
+    s.setValue(g, SETTING_PHYSCHAN, t->getElectrodeReadoutPhysChan());
+    s.setValue(g, SETTING_FREQ, t->getElectrodeReadoutRate());
 
-    s.setValue(SETTINGSGROUP_ACQUISITION, SETTING_OUTPUTPATH, optrode().getOutputDir());
-    s.setValue(SETTINGSGROUP_ACQUISITION, SETTING_RUNNAME, optrode().getRunName());
+    g = SETTINGSGROUP_SHUTTER;
+    s.setValue(g, SETTING_PHYSCHAN, t->getShutterPulseCounter());
+    s.setValue(g, SETTING_DUTY, t->getShutterPulseDuty());
+    s.setValue(g, SETTING_FREQ, t->getShutterPulseFrequency());
+    s.setValue(g, SETTING_TERM, t->getShutterPulseTerm());
+    s.setValue(g, SETTING_ENABLED, t->getShutterPulseEnabled());
+
+    g = SETTINGSGROUP_TIMING;
+    s.setValue(g, SETTING_INITIALDELAY, t->getShutterInitialDelay());
+    s.setValue(g, SETTING_STIMDURATION, t->stimulationDuration());
+    s.setValue(g, SETTING_POSTSTIMULATION, optrode().getPostStimulation());
+
+    g = SETTINGSGROUP_ACQUISITION;
+    s.setValue(g, SETTING_OUTPUTPATH, optrode().getOutputDir());
+    s.setValue(g, SETTING_RUNNAME, optrode().getRunName());
 
     s.saveSettings();
 
@@ -146,32 +156,41 @@ void MainWindow::loadSettings()
     const Settings s = settings();
 
     Tasks *t = optrode().NITasks();
-    t->setMainTrigPhysChan(s.value(SETTINGSGROUP_MAINTRIG, SETTING_PHYSCHAN).toString());
-    t->setMainTrigTerm(s.value(SETTINGSGROUP_MAINTRIG, SETTING_TERM).toString());
+    QString g = SETTINGSGROUP_MAINTRIG;
+    t->setMainTrigPhysChan(s.value(g, SETTING_PHYSCHAN).toString());
+    t->setMainTrigTerm(s.value(g, SETTING_TERM).toString());
 
-    t->setLEDFreq(s.value(SETTINGSGROUP_LED1, SETTING_FREQ).toDouble());
-    t->setLED1PhysChan(s.value(SETTINGSGROUP_LED1, SETTING_PHYSCHAN).toString());
-    t->setLED1Term(s.value(SETTINGSGROUP_LED1, SETTING_TERM).toString());
-    t->setLED2PhysChan(s.value(SETTINGSGROUP_LED2, SETTING_PHYSCHAN).toString());
-    t->setLED2Term(s.value(SETTINGSGROUP_LED2, SETTING_TERM).toString());
+    g = SETTINGSGROUP_LED1;
+    t->setLEDFreq(s.value(g, SETTING_FREQ).toDouble());
+    t->setLED1PhysChan(s.value(g, SETTING_PHYSCHAN).toString());
+    t->setLED1Term(s.value(g, SETTING_TERM).toString());
 
-    t->setElectrodeReadoutPhysChan(s.value(SETTINGSGROUP_ELREADOUT, SETTING_PHYSCHAN).toString());
-    t->setElectrodeReadoutRate(s.value(SETTINGSGROUP_ELREADOUT, SETTING_FREQ).toDouble());
+    g = SETTINGSGROUP_LED2;
+    t->setLED2PhysChan(s.value(g, SETTING_PHYSCHAN).toString());
+    t->setLED2Term(s.value(g, SETTING_TERM).toString());
 
-    t->setShutterPulseCounter(s.value(SETTINGSGROUP_SHUTTER, SETTING_PHYSCHAN).toString());
-    t->setShutterPulseDuty(s.value(SETTINGSGROUP_SHUTTER, SETTING_DUTY).toDouble());
-    t->setShutterPulseFrequency(s.value(SETTINGSGROUP_SHUTTER, SETTING_FREQ).toDouble());
-    t->setShutterPulseTerm(s.value(SETTINGSGROUP_SHUTTER, SETTING_TERM).toString());
-    t->setShutterPulseEnabled(s.value(SETTINGSGROUP_SHUTTER, SETTING_ENABLED).toBool());
+    g = SETTINGSGROUP_ELREADOUT;
+    t->setElectrodeReadoutPhysChan(s.value(g, SETTING_PHYSCHAN).toString());
+    t->setElectrodeReadoutRate(s.value(g, SETTING_FREQ).toDouble());
 
-    t->setShutterInitialDelay(s.value(SETTINGSGROUP_TIMING, SETTING_INITIALDELAY).toDouble());
-    t->setStimulationDuration(s.value(SETTINGSGROUP_TIMING, SETTING_STIMDURATION).toDouble());
-    optrode().setPostStimulation(s.value(SETTINGSGROUP_TIMING, SETTING_POSTSTIMULATION).toDouble());
+    g = SETTINGSGROUP_SHUTTER;
+    t->setShutterPulseCounter(s.value(g, SETTING_PHYSCHAN).toString());
+    t->setShutterPulseDuty(s.value(g, SETTING_DUTY).toDouble());
+    t->setShutterPulseFrequency(s.value(g, SETTING_FREQ).toDouble());
+    t->setShutterPulseTerm(s.value(g, SETTING_TERM).toString());
+    t->setShutterPulseEnabled(s.value(g, SETTING_ENABLED).toBool());
 
-    optrode().setOutputDir(s.value(SETTINGSGROUP_ACQUISITION, SETTING_OUTPUTPATH).toString());
-    optrode().setRunName(s.value(SETTINGSGROUP_ACQUISITION, SETTING_RUNNAME).toString());
+    g = SETTINGSGROUP_TIMING;
+    t->setShutterInitialDelay(s.value(g, SETTING_INITIALDELAY).toDouble());
+    t->setStimulationDuration(s.value(g, SETTING_STIMDURATION).toDouble());
+    optrode().setPostStimulation(s.value(g, SETTING_POSTSTIMULATION).toDouble());
 
-    optrode().getBehaviorCamera()->setROI(s.value(SETTINGSGROUP_BEHAVCAMROI, SETTING_ROI).toRect());
+    g = SETTINGSGROUP_ACQUISITION;
+    optrode().setOutputDir(s.value(g, SETTING_OUTPUTPATH).toString());
+    optrode().setRunName(s.value(g, SETTING_RUNNAME).toString());
+
+    g = SETTINGSGROUP_BEHAVCAMROI;
+    optrode().getBehaviorCamera()->setROI(s.value(g, SETTING_ROI).toRect());
 
     QSettings settings;
 
